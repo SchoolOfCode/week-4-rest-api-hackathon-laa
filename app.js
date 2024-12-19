7084d608de3f14b8e4e757807e1caab2ae285ace
Luke Davies <luke.davies@junitech.com>
2024-12-19 13:36:44 +0000

    server runs

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
+// // THIS IS OUR ROOT DIRECTORY
+
+// import express from "express";
+
+// // import the gamesRouter
+// import gamesRouter from "./router.js";
+
+// // create the express application
+// const app = express();
+
+// app.use(express.json());
+
+// // Use the gamesRouter for any requests to the /games path
+// app.use("/games", gamesRouter);
+
+// // Export the app instance so it can be used in other files
+// export default app;
+
 import express from "express";
-
-// Import the gamesRouter
-import gamesRouter from "./router.js";
-
-// Create an instance of an Express application
 const app = express();
+const PORT = 3000;
+
+import { getGames } from "./functions.js";
 
 app.use(express.json());
 
-// Use the gamesRouter for any requests to the /games path
-app.use("/games", gamesRouter);
+app.get("/", function (req, res) {
+  res.send("Hi there! This is the steam games REST API");
+});
 
-// Export the app instance so it can be used in other files
-export default app;
+app.listen(PORT, function () {
+  console.log(`Server is now listening on http://localhost:${PORT}`);
+});
+
+app.get("/games", async function (req, res) {
+  // respond with the array of quotes
+  // this function would: get quotes, respond with them
+  const gamesArray = await getGames();
+  res.json(gamesArray);
+});
--- router.js
+++ router.js
@@ -1,17 +1,17 @@
-import express from "express";
-const app = express();
+// import express from "express";
+// const app = express();
 
-import { getGames } from "./functions.js";
+// import { getGames } from "./functions.js";
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get("/games", async function (req, res) {
-  // respond with the array of quotes
-  // this function would: get quotes, respond with them
-  const gamesArray = await getGames();
-  res.json(gamesArray);
-});
+// app.get("/games", async function (req, res) {
+//   // respond with the array of quotes
+//   // this function would: get quotes, respond with them
+//   const gamesArray = await getGames();
+//   res.json(gamesArray);
+// });
 
-const router = express.Router();
+// const gamesRouter = express.Router();
 
-export default router;
+// export default gamesRouter;
